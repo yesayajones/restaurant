965a54f158376f617b465708e16597c8cdaba6f1
Yesaya Jones <yesayajones@gmail.com>
2023-08-21 09:59:51 +0200

    chore: Remove the FooterOverlay and Newsletter components

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import { FiFacebook, FiTwitter, FiInstagram } from 'react-icons/fi';
 
-import { FooterOverlay, Newsletter } from '../../components';
 import { images } from '../../constants';
 import './Footer.css';
 
 const Footer = () => (
 	<div className='app__footer section__padding' id='login'>
-		<FooterOverlay />
-		<Newsletter />
 
 		<div className='app__footer-links'>
 			<div className='app__footer-links_contact'>
