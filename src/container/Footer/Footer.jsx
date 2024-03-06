a66249b7851e046572cd39dff0cb0e06dac801f6
Yesaya Jones <yesayajones@gmail.com>
2024-03-06 08:31:14 -0600

    chore: Update address on footer

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -10,7 +10,7 @@ const Footer = () => (
 		<div className='app__footer-links'>
 			<div className='app__footer-links_contact'>
 				<h1 className='app__footer-headtext'>Contact Us</h1>
-				<p className='p__opensans'>9 W 53rd St, New York, NY 10019, USA</p>
+				<p className='p__opensans'> 500 Belvedere St, New York, NY 10019, USA</p>
 				<p className='p__opensans'>+1 212-344-1230</p>
 				<p className='p__opensans'>+1 212-555-1230</p>
 			</div>
